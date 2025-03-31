b354f942d343f6d406cb06b97ce22e0ffbc930ce
esteban1318 <poveda1318@gmail.com>
2025-03-30 19:07:15 -0500

    modificaciones db.js

--- src/db.js
+++ src/db.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'ballast.proxy.rlwy.net', // Host de Railway
-  user: process.env.MYSQL_USER || 'root',     // Usuario correcto
-  password: process.env.MYSQL_PASSWORD || 'bkgjXZqXcmXDdUuEPIylionwtbNfBglv', // Contraseña de Railway
-  database: process.env.MYSQL_DATABASE || 'conjunto_residencial', // Asegura que sea esta BD
-  port: process.env.MYSQL_PORT || 3306,       // Puerto de Railway
+  host: process.env.MYSQL_HOST || 'ballast.proxy.rlwy.net',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'bkgjXZqXcmXDdUuEPIylionwtbNfBglv',
+  database: process.env.MYSQL_DATABASE || 'conjunto_residencial',
+  port: process.env.MYSQL_PORT || 55687,
   waitForConnections: true,
   connectionLimit: 10,
+  authPlugins: { 
+    caching_sha2_password: () => require('mysql2/lib/auth_plugins').caching_sha2_password
+  }
 });
 
 module.exports = pool;
