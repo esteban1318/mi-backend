34be90da9f6398fa7e40ce9a41ca932af5f6e207
esteban1318 <poveda1318@gmail.com>
2025-04-03 11:55:37 -0500

    se modifica variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,13 +1,15 @@
-require('dotenv').config(); // Cargar variables de entorno
-
-const mysql = require('mysql2/promise');
+require('dotenv').config();
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = pool;
+module.exports = pool.promise();
